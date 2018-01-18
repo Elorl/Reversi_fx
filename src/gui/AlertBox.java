6de54213aee773df2a36fae925e003e1b5620ca5
Elorl <elor14@gmail.com>
2018-01-18 19:30:43 +0200

    add design to the alertBox

--- src/gui/AlertBox.java
+++ src/gui/AlertBox.java
@@ -1,5 +1,7 @@
 package gui;
 
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.stage.*;
 import javafx.scene.*;
 import javafx.scene.layout.*;
@@ -24,19 +26,23 @@ public class AlertBox {
         window.initModality(Modality.APPLICATION_MODAL);
         window.setTitle("Alert");
         //set size to the window
-        window.setHeight(100);
+        window.setHeight(120);
         window.setWidth(400);
         Label label = new Label();
+        msg = "\n" + msg + "\n";
         label.setText(msg);
+        label.setTextFill(Color.WHITE);
         Button closeButton = new Button("Ok, got it!");
         closeButton.setOnAction(e -> window.close());
-
-        VBox layout = new VBox(20);
-        layout.getChildren().addAll(label, closeButton);
-        layout.setAlignment(Pos.CENTER);
-
+        closeButton.setStyle("-fx-background-radius: 6; -fx-background-color:linear-gradient(#002444, #144C7D);  -fx-effect: dropshadow( three-pass-box , rgba(255,255,255,0.2) , 10, 0.0 , 0 , 5);");
+        closeButton.setTextFill(Color.WHITE);
+        //create the new vbox.
+        VBox vBOX = new VBox(20);
+        vBOX.getChildren().addAll(label, closeButton);
+        vBOX.setAlignment(Pos.CENTER);
+        vBOX.setStyle("-fx-background-image: url(wallpaper.jpg);");
         //Display window and wait for it to be closed before returning
-        Scene scene = new Scene(layout);
+        Scene scene = new Scene(vBOX);
         window.setScene(scene);
         window.showAndWait();
     }
